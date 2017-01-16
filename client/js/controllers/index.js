55964e21040df49cd91d67763b2823be3c380a08
lishen-kingsoft <lishen@kingsoft.com>
2017-01-16 17:03:45 +0800

    refine logic
    Signed-off-by: lishen-kingsoft <lishen@kingsoft.com>

--- client/js/controllers/index.js
+++ client/js/controllers/index.js
@@ -5,5 +5,5 @@ import ManagementController from './management-controller';
 
 export default angular
   .module('luckydraw3.controllers', [])
-  .controller('DrawController', DrawController)
-  .controller('ManagementController', ManagementController);
+  .controller('DrawController', ['$scope', '$meteor', '$reactive', '$timeout', '$interval', DrawController])
+  .controller('ManagementController', ['$scope', '$meteor', '$reactive', ManagementController]);
