f48b348b64b74adfe2c54a4b98deb1bc89965d8c
lishen-kingsoft <lishen@kingsoft.com>
2017-01-17 20:01:11 +0800

    refine logic

--- imports/api/ppls.js
+++ imports/api/ppls.js
@@ -4,15 +4,12 @@ import { Mongo } from 'meteor/mongo';
 import { Configs } from '/imports/api/configs';
 
 import { defaultPpls } from '/imports/mock/default-ppls';
- 
+
 export const Ppls = new Mongo.Collection('ppls');
 
 Ppls.const = {
-  DRAW_PPL_COUNT: 350,
-  LOC_ZHUHAI: '珠海',
-  LOC_GUANGZHOU: '广州',
-  LOC_XIANGGANG: '香港'
-}
+  DRAW_PPL_COUNT: 350
+};
 
 Meteor.methods({
   'removeAllPpls' : function () {
@@ -26,15 +23,15 @@ Meteor.methods({
 
 if (Meteor.isServer) {
   const RawPpls = Ppls.rawCollection();
-  
+
   Meteor.methods({
     'drawPpls': function(config) {
       RawPpls.aggregateSync = Meteor.wrapAsync(RawPpls.aggregate);
       var candidates = [];
       if (config.scope == Configs.const.SCOPE_LOCAL) {
-        candidates = RawPpls.aggregateSync([{$match: {laId: '', loc: {$in: [Ppls.const.LOC_ZHUHAI, Ppls.const.LOC_GUANGZHOU, Ppls.const.LOC_XIANGGANG]}}}, {$sample: {size: Ppls.const.DRAW_PPL_COUNT}}]);
+        candidates = RawPpls.aggregateSync([{$match: {laId: '', forLa: true}}, {$sample: {size: Ppls.const.DRAW_PPL_COUNT}}]);
       } else if(config.scope == Configs.const.SCOPE_GLOBAL) {
-        candidates = RawPpls.aggregateSync([{$match: {gaId: ''}}, {$sample: {size: Ppls.const.DRAW_PPL_COUNT}}]);
+        candidates = RawPpls.aggregateSync([{$match: {gaId: '', forGa: true}}, {$sample: {size: Ppls.const.DRAW_PPL_COUNT}}]);
       }
 
       if (candidates.length) {
